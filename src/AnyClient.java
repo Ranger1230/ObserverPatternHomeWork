90016d81425b76fd2811d43da3b5f5b248a40b5d
Chris <Chris@delusivegames.com>
2014-11-09 15:31:45 -0600

    added in some user friendly features.

--- src/AnyClient.java
+++ src/AnyClient.java
@@ -1,12 +1,24 @@
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JTextPane;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
 import java.awt.TextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -68,17 +80,19 @@ public class AnyClient implements Observer {
 				stockStatusInput.setText("");
 			}
 		});
-		
+		btnAddStock.setEnabled(false);
 		btnAddStock.setBounds(333, 243, 117, 29);
 		frame.getContentPane().add(btnAddStock);
 		
 		stockSymboleInput = new JTextField();
 		stockSymboleInput.setBounds(6, 242, 134, 28);
+		stockSymboleInput.getDocument().addDocumentListener(new ButtonDisabler(stockSymboleInput, btnAddStock));
 		frame.getContentPane().add(stockSymboleInput);
 		stockSymboleInput.setColumns(10);
 		
-		stockStatusInput = new JTextField();
+		stockStatusInput = new DoubleTextField();
 		stockStatusInput.setBounds(167, 242, 134, 28);
+		stockStatusInput.getDocument().addDocumentListener(new ButtonDisabler(stockStatusInput, btnAddStock));
 		frame.getContentPane().add(stockStatusInput);
 		stockStatusInput.setColumns(10);
 		
@@ -107,4 +121,86 @@ public class AnyClient implements Observer {
 	public void update(Observable o, Object arg) {
 		textArea.setText(o.toString());
 	}
+	
+	private static class ButtonDisabler implements DocumentListener{
+		private static List<JTextField> fields = new ArrayList<JTextField>();;
+		private static JButton _button;
+		
+		public ButtonDisabler(JTextField field, JButton button){
+			fields.add(field);
+			_button = button;
+		}
+		
+		protected void update() {
+			boolean enableButton = true;
+			for(JTextField jtf : fields){
+				if(jtf.getText().length() == 0){
+					enableButton = false;
+				}
+			}
+			_button.setEnabled(enableButton);
+        }
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            update();
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            update();
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            update();
+        }
+	}
+	
+	private class DoubleTextField extends JTextField {
+		  public DoubleTextField() {
+		    super();
+		  }
+
+		  protected Document createDefaultModel() {
+		    return new DoubleTextDocument();
+		  }
+
+		  public boolean isValid() {
+		    try {
+		      String inputText = this.getText();
+			  if(inputText == "" || inputText == null){
+				  return true;
+			  }
+		      Double.parseDouble(inputText);
+		      return true;
+		    } catch (Exception e) {
+		      return false;
+		    }
+		  }
+
+		  public double getValue() {
+		    try {
+		      return Double.parseDouble(getText());
+		    } catch (NumberFormatException e) {
+		      return 0;
+		    }
+		  }
+		  class DoubleTextDocument extends PlainDocument {
+		    public void insertString(int offs, String str, AttributeSet a)
+		        throws BadLocationException {
+		      if (str == null)
+		        return;
+		      String oldString = getText(0, getLength());
+		      String newString = oldString.substring(0, offs) + str
+		          + oldString.substring(offs);
+		      try {
+		    	  Double.parseDouble(newString + "0");
+		        super.insertString(offs, str, a);
+		      } catch (NumberFormatException e) {
+		      }
+		    }
+		  }
+
+		}
 }
